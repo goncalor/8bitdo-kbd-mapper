395a69ff144417da002d46beb0f1f564586bd4ed
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2024-04-24 12:44:46 +0100

    Start writing EightBDKdb class

--- 8bdkbd.py
+++ 8bdkbd.py
@@ -9,27 +9,49 @@ MAP = [0x52, 0xfa, 0x03, 0x0c, 0x00, 0xaa, 0x09, 0x71]
 MAP_DONE = [0x52, 0x76, 0xa5]
 
 
-def read(endpoint, size=64, timeout=1000):
-    return endpoint.read(size, timeout).tobytes()
+class EightBDKdb:
+
+    def __init__(self):
+        self.ep_read, self.ep_write = get_8bd_endpoints()
+        print(self.ep_read)
+        print(self.ep_write)
+
+    def read(self, size=64, timeout=1000):
+        return self.ep_read.read(size, timeout).tobytes()
+
+    def write(self, data, size=33):
+        return self.ep_write.write(data + [0] * (33 - len(data)))
+
+    def map_hid_usage(self, hwkey, usage):
+        #TODO: verify data length
+        #TODO: verify key value makes sense
+        self.write(ATTN)
+        print(self.read().hex())
+        self.write(MAP + [hwkey] + usage)
+        print(self.read().hex())
+        self.write(MAP_DONE)
+        print(self.read().hex())
+
+    def map_key(self, hwkey, key):
+        pass
 
 
-def write(endpoint, data, size=33):
-    return endpoint.write(data + [0] * (33 - len(data)))
+def get_8bd_endpoints():
+    dev = usb.core.find(idVendor=0x2dc8, idProduct=0x5200)
 
+    if dev is None:
+        raise ValueError("Could not find 8BitDo Retro Mechanical Keyboard")
 
-def map_hid_usage(epw, epr, hwkey, usage):
-    #TODO: verify data length
-    #TODO: verify key value makes sense
-    write(epw, ATTN)
-    print(read(epr).hex())
-    write(epw, MAP + [hwkey] + usage)
-    print(read(epr).hex())
-    write(epw, MAP_DONE)
-    print(read(epr).hex())
+    # detach interface #2 if needed
+    if dev.is_kernel_driver_active(2):
+        print("detaching kernel driver")
+        dev.detach_kernel_driver(2)
 
+    # [config][(interface, alternate)][endpoint]
+    endpoint_in = dev[0][(2, 0)][0]
+    endpoint_out = dev[0][(2, 0)][1]
 
-def map_key(endpoint, hwkey, key):
-    pass
+    return endpoint_in, endpoint_out
 
 
 parser = argparse.ArgumentParser(
@@ -50,16 +72,5 @@ parser_map.add_argument("--hid",
 
 args = parser.parse_args()
 
-dev = usb.core.find(idVendor=0x2dc8, idProduct=0x5200)
-
-if dev is None:
-    raise ValueError("Could not find 8BitDo Retro Mechanical Keyboard")
-
-# detach interface #2 if needed
-if dev.is_kernel_driver_active(2):
-    print("detaching kernel driver")
-    dev.detach_kernel_driver(2)
-
-# [config][(interface, alternate)][endpoint]
-endpoint_in = dev[0][(2, 0)][0]
-endpoint_out = dev[0][(2, 0)][1]
+test = EightBDKdb()
+test.map_hid_usage(0x29, [0x07, 0x00, 0x29])
